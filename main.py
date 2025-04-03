9c5cba8aa713b8001a557ac997664ae02a9d1e83
pkihiu <p.kihiu@alustudent.com>
2025-04-03 15:18:49 +0300

    update main

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ tf.get_logger().setLevel('ERROR')
 UPLOAD_FOLDER = "uploaded_data"
 VISUALIZATION_DIR = "visualizations"
 MODEL_DIR = "models"
-KERAS_PATH = os.path.join(MODEL_DIR, "Model_plant.keras")
+KERAS_PATH = os.path.join(MODEL_DIR, "plant_disease_model.keras")
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'zip'}
 MAX_IMAGES_PER_BATCH = 20
 
